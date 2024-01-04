6bc3082f54ba85b90a157da0da3bc35c40b2b09b
ZachariePage <zachariepag@yahoo.com>
2024-01-04 17:25:10 -0500

    Update svelte.config.js

--- svelte.config.js
+++ svelte.config.js
@@ -1,18 +1,14 @@
 import adapter from '@sveltejs/adapter-static';
-import {
-    vitePreprocess
-} from '@sveltejs/kit/vite';
-
-/** @type {import('@sveltejs/kit').Config} */
-const config = {
-    preprocess: vitePreprocess(),
-
-    kit: {
-        adapter: adapter(),
-        paths: {
-          base: process.env.NODE_ENV === 'production' ? '/sveltekit-github-pages' : '',
-      }
-    }
+export default {
+	kit: {
+		adapter: adapter({
+			// default options are shown. On some platforms
+			// these options are set automatically — see below
+			pages: 'build',
+			assets: 'build',
+			fallback: undefined,
+			precompress: false,
+			strict: true
+		})
+	}
 };
-
-export default config;
