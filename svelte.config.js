532a85b7c3873f66e501e1f5262acb5cad3e43de
ZachariePage <zachariepag@yahoo.com>
2024-01-07 10:13:46 -0500

    c

--- svelte.config.js
+++ svelte.config.js
@@ -1,17 +1,19 @@
 import adapter from '@sveltejs/adapter-static';
-export default {
-	kit: {
-		adapter: adapter({
-			// default options are shown. On some platforms
-			// these options are set automatically — see below
-			pages: 'build',
-			assets: 'build',
-			fallback: 'index.html',
-			precompress: false,
-			strict: false
-		}),
-		paths: {
-			base: process.argv.includes('dev') ? '' : process.env.BASE_PATH
-		}
-	}
+/** @type {import('@sveltejs/kit').Config} */
+const config = {
+    kit: {
+        adapter: adapter({
+            // default options are shown. On some platforms
+            // these options are set automatically — see below
+            pages: 'build',
+            assets: 'build',
+            fallback: 'index.html',
+            precompress: false,
+            strict: false
+        }),
+        paths:{
+            base: process.argv.includes('dev') ? '' : '/PortfolioZpg'
+        }
+    }
 };
+export default config;
